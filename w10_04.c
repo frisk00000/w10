f12ab2093600eea08ac3f3e71a0a062e467575e8
frisk00000 <okd.ren0919@gmail.com>
2024-12-28 18:25:49 +0900

    Update w10_04.c

--- w10_04.c
+++ w10_04.c
@@ -1,18 +1,26 @@
+/*∫0→1 x^2dxを計算する*/
 #include <stdio.h>
 #include <math.h>
 
 int main()
 {
+  int i, j;
+  int N;
   int a = 0;
   int b = 1;
-  int N, h, j, I;
+  double ans, h;
 
-  for (j = 1; j <= N - 1; j++)
+  printf("N, integral\n");
+
+  for (i = 1; i <= 20; i++)
   {
-    N = pow(2, j);
-    h = (b - a) / N;
-    I = (a + j * h) * (a + j * h) * h
-
-    printf("%d, %.15lf");
+    N = pow(2, i);
+    h = (double)(b - a) / N;
+    ans = 0.0;
+    for (j = 0; j <= N - 1; j++)
+    {
+      ans += (a + j * h) * (a + j * h) * h;
+    }
+    printf("%d, %.15lf\n", N, ans);
   }
 }
